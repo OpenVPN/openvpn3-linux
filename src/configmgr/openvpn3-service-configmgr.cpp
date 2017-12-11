9652d29996f6ba69a2d5962d266b228eade5a6d1
David Sommerseth <davids@openvpn.net>
2017-12-11 18:08:03 +0100

    copyright: Moving from GPLv3 to AGPLv3
    The OpenVPN 3 Core library will move back to AGPLv3. And to keep
    licensing easy and not confusing, this Linux client will also be AGPLv3
    even though not strictly needed.  This move is also not considered
    to bring any drawbacks either, so we go for simplicity.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/configmgr/openvpn3-service-configmgr.cpp
+++ src/configmgr/openvpn3-service-configmgr.cpp
@@ -4,16 +4,17 @@
 //  Copyright (C) 2017      David Sommerseth <davids@openvpn.net>
 //
 //  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, version 3 of the License
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, version 3 of the
+//  License.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
+//  GNU Affero General Public License for more details.
 //
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #include "dbus/core.hpp"
