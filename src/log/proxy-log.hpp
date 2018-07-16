a192329d7a542e2afc32c6f23f0e138e007cf58f
David Sommerseth <davids@openvpn.net>
2018-07-16 16:44:07 +0200

    log: Implement proxy interface for the Log Service
    This implements a C++ API for the net.openvpn.v3.log service (provided
    by openvpn3-service-logger --service).  It also includes a simple test
    program to manipulate settings in the log service and test sending Log
    signals.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

---
+++ src/log/proxy-log.hpp
@@ -0,0 +1,146 @@
+//  OpenVPN 3 Linux client -- Next generation OpenVPN client
+//
+//  Copyright (C) 2018         OpenVPN, Inc. <sales@openvpn.net>
+//  Copyright (C) 2018         David Sommerseth <davids@openvpn.net>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, version 3 of the
+//  License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file   proxy-log.hpp
+ *
+ * @brief  D-Bus proxy interface to the net.openvpn.v3.log service
+ */
+
+#pragma once
+
+#include <openvpn/common/rc.hpp>
+
+#include "dbus/core.hpp"
+#include "dbus/proxy.hpp"
+
+/**
+ *  Client proxy implementation interacting with a
+ *  the net.openvpn.v3.log service
+ */
+class LogServiceProxy : public DBusProxy,
+                        public RC<thread_unsafe_refcount>
+
+{
+public:
+    typedef RCPtr<LogServiceProxy> Ptr;
+
+    /**
+     *  Initialize the LogServiceProxy.
+     *
+     * @param dbuscon  D-Bus connection to use for the proxy calls
+     */
+    LogServiceProxy(GDBusConnection * dbuscon)
+        : DBusProxy(dbuscon,
+                    OpenVPN3DBus_name_log,
+                    OpenVPN3DBus_interf_log,
+                    OpenVPN3DBus_rootp_log)
+    {
+    }
+
+
+    /**
+     *  Attach this D-Bus connection to the log service.  By doing this
+     *  all Log signals sent from this client will be caught by the log
+     *  service.
+     *
+     * @param interf  String containing the D-Bus interface the log service
+     *                subscribe to.
+     *
+     */
+    void Attach(const std::string interf)
+    {
+        Call("Attach", g_variant_new("(s)", interf.c_str()), true);
+    }
+
+
+    /**
+     *  Detach this running connection from the log service.  This will
+     *  make the log service unsubscribe from the provided interface.
+     *
+     * @param interf  String containing the D-Bus interface to unsubscribe
+     *                from.
+     */
+    void Detach(const std::string interf)
+    {
+        Call("Detach", g_variant_new("(s)", interf.c_str()), true);
+    }
+
+
+    /**
+     *  Retrieve the number of subscriptions the log service is attached to
+     *
+     * @return  Returns an unsigned integer with number of subscribers.
+     */
+    unsigned int GetNumAttached()
+    {
+        return GetUIntProperty("num_attached");
+    }
+
+
+    /**
+     *  Retrieve the current log level (verbosity) of the log service.  This
+     *  is a global setting for all subscriptions.
+     *
+     * @return  Returns an unsigned int of the log level (0-6).
+     */
+    unsigned int GetLogLevel()
+    {
+        return GetUIntProperty("log_level");
+    }
+
+
+    /**
+     *  Modifies the log level the log service will use for filtering out
+     *  log messages.
+     *
+     * @param loglvl  Unsigned int of the new log level to use.
+     */
+    void SetLogLevel(unsigned int loglvl)
+    {
+        SetProperty("log_level", loglvl);
+    }
+
+
+    /**
+     *  Will log entries carry a timestamp?
+     *
+     * @return  Returns true if log events will be prefixed with at
+     *          timestamp.
+     */
+    bool GetTimestampFlag()
+    {
+        return GetBoolProperty("timestamp");
+    }
+
+
+    /**
+     *  Flips the timestamp flag.  By setting this to false, no timestamps
+     *  will be added to log entries.  This is useful if log events are
+     *  being processed by another log processing which already adds this
+     *  information.
+     *
+     * @param tstamp  Boolean flag wheter to enable (true) or disable (false)
+     *                the log timestamp.
+     */
+    void SetTimestampFlag(bool tstamp)
+    {
+        SetProperty("timestamp", tstamp);
+    }
+};
---
+++ src/tests/dbus/logservice1.cpp
@@ -0,0 +1,162 @@
+//  OpenVPN 3 Linux client -- Next generation OpenVPN client
+//
+//  Copyright (C) 2018         OpenVPN, Inc. <sales@openvpn.net>
+//  Copyright (C) 2018         David Sommerseth <davids@openvpn.net>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, version 3 of the
+//  License.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+/**
+ * @file   logservice1.cpp
+ *
+ * @brief  Test program for the log service and its proxy interface
+ */
+
+#include <iostream>
+
+#include "common/cmdargparser.hpp"
+#include "dbus/core.hpp"
+#include "dbus/signals.hpp"
+#include "log/dbus-log.hpp"
+#include "log/proxy-log.hpp"
+
+
+int cmd_props(ParsedArgs args)
+{
+    DBus dbus(G_BUS_TYPE_SYSTEM);
+    dbus.Connect();
+    LogServiceProxy logsrvprx(dbus.GetConnection());
+
+    if (args.Present("log-level"))
+    {
+        logsrvprx.SetLogLevel(std::atoi(args.GetValue("log-level", 0).c_str()));
+    }
+
+    if (args.Present("timestamp"))
+    {
+        logsrvprx.SetTimestampFlag(args.GetBoolValue("timestamp", 0));
+    }
+
+    std::cout << "Number of attached loggers: " << logsrvprx.GetNumAttached() << std::endl;
+    std::cout << "Log level: " << logsrvprx.GetLogLevel() << std::endl;
+    std::cout << "Timestamp enabled? " << (logsrvprx.GetTimestampFlag() ? "yes" : "no") << std::endl;
+
+    return 0;
+}
+
+
+int cmd_send(ParsedArgs args)
+{
+    DBus dbus(G_BUS_TYPE_SYSTEM);
+    dbus.Connect();
+
+    if (!args.Present("group"))
+    {
+        throw CommandException("send", "Missing required --group");
+    }
+    unsigned int lgrp = std::atoi(args.GetValue("group", 0).c_str());
+
+    if (!args.Present("category"))
+    {
+        throw CommandException("send", "Missing required --category");
+    }
+    unsigned int lctg = std::atoi(args.GetValue("category", 0).c_str());
+
+    std::string path = (args.Present("object-path") ? args.GetValue("object-path", 0) : "/net/openvpn/v3/logtest");
+    std::string intf = (args.Present("interface") ? args.GetValue("interface", 0) : "net.openvpn.v3.logtest");
+
+    auto extra = args.GetAllExtraArgs();
+    std::string msg = "(empty message)";
+    if (extra.size() > 0)
+    {
+        msg = "";
+        for (const auto& t : extra)
+        {
+            msg += t + " ";
+        }
+    }
+    std::cout << "     Path: " << path << std::endl;
+    std::cout << "Interface: " << intf << std::endl;
+    std::cout << "Log event: " << msg << std::endl;
+    try
+    {
+        LogServiceProxy logsrvprx(dbus.GetConnection());
+        if (args.Present("attach"))
+        {
+            std::cout << "Attatching log" << std::endl;
+            logsrvprx.Attach(intf);
+            sleep(1); // Wait for the logger to settle this new attachment
+        }
+
+        LogSender sig(dbus.GetConnection(), (LogGroup) lgrp, intf, path);
+        sig.Send("Log", g_variant_new("(uus)", lgrp, lctg, msg.c_str()));
+        std::cout << "Log signal sent" << std::endl;
+
+        if (args.Present("attach"))
+        {
+            sleep(1); // Wait for the logger to process the Log signal
+            std::cout << "Detaching log" << std::endl;
+            logsrvprx.Detach(intf);
+        }
+
+    }
+    catch (std::exception& err)
+    {
+        std::cerr << err.what() << std::endl;
+        return 1;
+    }
+    return 0;
+
+}
+
+
+int main(int argc, char **argv)
+{
+    Commands cmds("openvpn3-service-logger service tester",
+                  "Simple program to test the interfaces exposted by "
+                  "openvpn3-service-logger (net.openvpn.v3.log)");
+
+    auto props = cmds.AddCommand("props", "Gets and sets properties",
+                                 cmd_props);
+    props->AddOption("log-level", 'l', "LEVEL", true,
+                     "Sets the log verbosity");
+    props->AddOption("timestamp", 't', "BOOLEAN", true,
+                     "Sets the timestamp flag for log events. Valid values: true, false");
+
+    auto send = cmds.AddCommand("send", "Sends log events", cmd_send);
+    send->AddOption("attach", 'a', "Do an Attach() method call before sending log event");
+    send->AddOption("object-path", 'o', "PATH", true,
+                    "D-Bus path to use as the signal origin (default: /net/openvpn/v3/logtest)");
+    send->AddOption("interface", 'i', "STRING", true,
+                    "Interface string to use when sending log events (default: net.openvpn.v3.logtest");
+    send->AddOption("group", 'g', "INTEGER", true,
+                    "LogGroup value to use for the log event");
+    send->AddOption("category", 'c', "INTEGER", true,
+                    "LogCategory value to use for the log event");
+
+    try
+    {
+        return cmds.ProcessCommandLine(argc, argv);
+    }
+    catch (CommandException& e)
+    {
+        if (e.gotErrorMessage())
+        {
+            std::cerr << e.getCommand() << ": ** ERROR ** " << e.what() << std::endl;
+        }
+        return 9;
+    }
+}
+
+
