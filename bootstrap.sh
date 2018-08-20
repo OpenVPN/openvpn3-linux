eb95a6a7df1514d30d2617ad36ebe234c25c5689
Arne Schwabe <arne@openvpn.net>
2018-08-20 18:23:14 +0200

    Check for pkg-config in bootstrap.sh
    if pkg-config is not installed you get the extremely obscure error from
    autoconf
    
      error: possibly undefined macro: AC_MSG_ERROR
    
    error out in bootstrap.sh to give a better diagnostic
    
    Signed-off-by: Arne Schwabe <arne@openvpn.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,16 @@
 
 set -eu
 
+# if pkg-config is not installed you get the extremely obscure error from autoconf
+#   error: possibly undefined macro: AC_MSG_ERROR
+# error out here to make this a bit easier
+
+if ! pkg-config --version > /dev/null; then
+ echo "pkg-config is required";
+ exit 1
+fi
+
+
 echo "** Initializing git submodules ..."
 git submodule init
 git submodule update
