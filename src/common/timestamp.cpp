e135f3c399a51fe6d32b74a359a439474a88882e
David Sommerseth <davids@openvpn.net>
2018-11-11 22:27:21 +0100

    common: Make timestamp.hpp independent compile unit
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/common/timestamp.cpp
+++ src/common/timestamp.cpp
@@ -1,6 +1,6 @@
 //  OpenVPN 3 Linux client -- Next generation OpenVPN client
 //
-//  Copyright (C) 2018         OpenVPN Inc <sales@openvpn.net>
+//  Copyright (C) 2018         OpenVPN, Inc. <sales@openvpn.net>
 //  Copyright (C) 2018         David Sommerseth <davids@openvpn.net>
 //
 //  This program is free software: you can redistribute it and/or modify
@@ -18,12 +18,33 @@
 //
 
 /**
- * @file   timestamp.hpp
+ * @file   timestamp.cpp
  *
  * @brief  Simple functions for retriveving time/date related information
- *
  */
 
-#pragma once
+#include <sstream>
+#include <iomanip>
 
-std::string GetTimestamp();
+
+/**
+ *  Get a timestamp of the current date and time.  The format is
+ *  the ISO standard without time zone - YYYY-MM-DD HH:MM:SS
+ *
+ * @return  Returns a string with the date and time
+ */
+std::string GetTimestamp()
+{
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+
+    std::stringstream ret;
+    ret << 1900 + ltm->tm_year
+        << "-" << std::setw(2) << std::setfill('0') << 1 + ltm->tm_mon
+        << "-" << std::setw(2) << std::setfill('0') << ltm->tm_mday
+        << " " << std::setw(2) << std::setfill('0') << ltm->tm_hour
+        << ":" << std::setw(2) << std::setfill('0') << ltm->tm_min
+        << ":" << std::setw(2) << std::setfill('0') << ltm->tm_sec
+        << " ";
+    return ret.str();
+}
