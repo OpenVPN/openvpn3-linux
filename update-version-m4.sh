0ff9102fd0af9b6dbdeb77724150f9ec0801810c
David Sommerseth <davids@openvpn.net>
2019-02-27 16:04:37 +0100

    build: Be stricter about what a version tag is
    Before this change, any tag reference starting was 'v' was considered a
    version tag.  This would cause version references to be derived out from
    a tag reference later on.  In these cases we want to go back to the
    $branch_$commit based reference instead.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- update-version-m4.sh
+++ update-version-m4.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #  OpenVPN 3 Linux client -- Next generation OpenVPN client
 #
-#  Copyright (C) 2017      OpenVPN Inc. <sales@openvpn.net>
-#  Copyright (C) 2017      David Sommerseth <davids@openvpn.net>
+#  Copyright (C) 2017 - 2019  OpenVPN Inc. <sales@openvpn.net>
+#  Copyright (C) 2017 - 2019  David Sommerseth <davids@openvpn.net>
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-set -eu
+set -u
 
 #
 #  Retrieve the version based on the git tree
@@ -26,7 +26,13 @@ set -eu
 #  is used.  Otherwise compose a version string based on branch name
 #  and commit reference
 VERSION="$(git describe --always --tags)"
-if [ "$(echo ${VERSION} | cut -b-1)" != "v" ]; then
+
+#  Only accept explicit tag references, not a reference
+#  derived from a tag reference, such as "v3_beta-36-gcd68aee"
+echo ${VERSION} | grep -qE "^v[[:digit:]](|_[[:alnum:]]+)$"
+ec=$?
+set -eu
+if [ $ec -ne 0  ]; then
 	# Presume not a version tag, so use commit reference
 	VERSION="$(git rev-parse --symbolic-full-name HEAD | cut -d/ -f3-)_$(git rev-parse --short=16 HEAD)"
 	GUIVERSION="$(echo $VERSION | sed 's/_/:/')"
