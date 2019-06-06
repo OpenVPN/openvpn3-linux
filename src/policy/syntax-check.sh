e7120a3e9602982a2d5386eb52fb12a14e594107
David Sommerseth <davids@openvpn.net>
2019-06-06 20:51:48 +0200

    tests: Adding unit-test framework
    This adds a unit-test framework based on googletest (gtest).  Right now
    only the bare framework is introduced, not running any particular tests.
    
    This also moves the D-Bus policy syntax check to the main Makefile.am to
    avoid running multiple test scripts via 'make', which did look a bit
    confusing.  This required a slight modification to the syntax-check.sh
    script, where a lacking copyright header was added as well.
    
    In addition, the BUILD_TESTS declaration has been changed to
    BUILD_TEST_PROGS, as it is possible to build unit-tests without the
    additional test programs and vice versa.  The default is to build
    everything.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/policy/syntax-check.sh
+++ src/policy/syntax-check.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+#
+#  Copyright (C) 2018 - 2019  OpenVPN Inc. <sales@openvpn.net>
+#  Copyright (C) 2018 - 2019  David Sommerseth <davids@openvpn.net>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, version 3 of the
+#  License.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 
 set -eu
 
-for f in *.conf;
+for f in "$(dirname $0)"/*.conf;
 do
 	xmllint --format $f > /dev/null
 done
