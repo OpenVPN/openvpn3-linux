3b9fec4c7a827024b3efb6f6bd8e3c22b93180c4
David Sommerseth <davids@openvpn.net>
2020-06-03 00:51:36 +0200

    ovpn3cli/log: Improve the complete session log setup logic
    This reworks the complete session log event setup to consider the
    SESS_CREATED events from the session manager if a VPN session cannot
    via config name lookup.  This also moves the SESS_DESTROYED logic out
    of the SessionLogger and into this new LogAttach setup.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/ovpn3cli/commands/log.cpp
+++ src/ovpn3cli/commands/log.cpp
@@ -31,6 +31,7 @@
 #include "log/proxy-log.hpp"
 #include "configmgr/proxy-configmgr.hpp"
 #include "sessionmgr/proxy-sessionmgr.hpp"
+#include "sessionmgr/sessionmgr-events.hpp"
 #include "../arghelpers.hpp"
 
 using namespace openvpn;
@@ -161,6 +162,266 @@ private:
     GMainLoop *main_loop;
 };
 
+
+
+/**
+ *  Helper class to attach to log events from sessions
+ *
+ *  This class implements logic to also wait until the session manager
+ *  signals a newly created session when the log attach is tied to a
+ *  configuration profile name.  Once the session is found, the SessionLogger
+ *  class takes over the log event handling itself.  The LogAttach object
+ *  will also stop the logging once the session manager signals the session
+ *  has been destroyed.
+ *
+ */
+class LogAttach : public DBusSignalSubscription
+{
+public:
+    LogAttach(GMainLoop *main_loop,
+              GDBusConnection *dbuscon)
+        : DBusSignalSubscription(dbuscon,
+                                 OpenVPN3DBus_name_sessions,
+                                 OpenVPN3DBus_interf_sessions,
+                                 OpenVPN3DBus_rootp_sessions),
+        mainloop(main_loop)
+    {
+        manager.reset(new OpenVPN3SessionProxy(G_BUS_TYPE_SYSTEM,
+                                               OpenVPN3DBus_rootp_sessions));
+        Subscribe("SessionManagerEvent");
+    }
+
+
+    void AttachByPath(const std::string path)
+    {
+        session_path = path;
+        create_session_proxy(session_path);
+    }
+
+
+    void AttchByConfig(const std::string config)
+    {
+        config_name = config;
+        lookup_config_name(config_name);
+        create_session_proxy(session_path);
+    }
+
+
+    void AttachByInterface(const std::string interf)
+    {
+        tun_interf = interf;
+        lookup_interface(tun_interf);
+        create_session_proxy(session_path);
+    }
+
+
+    void SetLogLevel(const unsigned int loglvl)
+    {
+        log_level = loglvl;
+    }
+
+
+    void callback_signal_handler(GDBusConnection *connection,
+                                 const std::string sender_name,
+                                 const std::string object_path,
+                                 const std::string interface_name,
+                                 const std::string signal_name,
+                                 GVariant *parameters) override
+    {
+        if ("SessionManagerEvent" == signal_name)
+        {
+            SessionManager::Event ev(parameters);
+
+            switch (ev.type)
+            {
+            case SessionManager::EventType::SESS_CREATED:
+                if (session_proxy)
+                {
+                    // If we already have a running session proxy,
+                    // ignore new sessions
+                    return;
+                }
+                if (!config_name.empty())
+                {
+                    lookup_config_name(config_name);
+                }
+                else if (!tun_interf.empty())
+                {
+                    lookup_interface(tun_interf);
+                }
+
+                create_session_proxy(session_path);
+                break;
+
+            case SessionManager::EventType::SESS_DESTROYED:
+                if (0 != ev.path.compare(session_path))
+                {
+                    // This event is not related to us, ignore it
+                    return;
+                }
+
+                cleanup();
+                std::cout << "Session closed" << std::endl;
+                g_main_loop_quit((GMainLoop *) mainloop);
+                break;
+
+            case SessionManager::EventType::UNSET:
+            default:
+                // Ignore unknown types silently
+                break;
+            }
+        }
+    }
+
+private:
+    GMainLoop *mainloop = nullptr;
+    std::string session_path{""};
+    std::string config_name{""};
+    std::string tun_interf{""};
+    std::unique_ptr<OpenVPN3SessionProxy> manager = nullptr;
+    std::unique_ptr<OpenVPN3SessionProxy> session_proxy = nullptr;
+    SessionLogger::Ptr session_log = nullptr;
+    unsigned int log_level = 0;
+    bool log_flag_reset = false;
+    bool wait_notification = false;
+
+
+    void lookup_config_name(const std::string cfgname)
+    {
+        // We need to try a few times, as the SESS_CREATED event comes
+        // quite early and the session object itself might not be registered
+        // quite yet.  This needs to be a tight loop to catch the earliest
+        // log messages, as certificate only based authentication may do
+        // the full connection establishing in a little bit less than 200ms.
+        for (unsigned int i = 250; i > 0; i--)
+        {
+            std::vector<std::string> paths = manager->LookupConfigName(cfgname);
+            if (1 < paths.size())
+            {
+                throw CommandException("log",
+                                       "More than one session with the given "
+                                       "configuration profile name was found.");
+            }
+            else if (1 == paths.size())
+            {
+                // If only a single path is found, that's the one we're
+                // looking for.
+                session_path = paths.at(0);
+                return;
+            }
+            else
+            {
+                // If no paths has been found, we wait until next call
+                if (!wait_notification)
+                {
+                    std::cout << "Waiting for session to start ..."
+                              << std::flush;
+                    wait_notification = true;
+                }
+            }
+            usleep(10000); // 10ms
+        }
+    }
+
+
+    void lookup_interface(const std::string interf)
+    {
+        // This method does not have any retry logic as @lookup_config_name()
+        // the tun interface name will appear quite late in the connection
+        // logic, making it less useful for non-established VPN sessions.
+        // The tun device name is first known after a successful connection.
+        try
+        {
+            session_path = manager->LookupInterface(interf);
+        }
+        catch (const TunInterfaceException& excp)
+        {
+            throw CommandException("log", std::string(excp.GetRawError()));
+        }
+        catch (const std::exception& excp)
+        {
+            throw CommandException("log", std::string(excp.what()));
+        }
+    }
+
+
+    /**
+     *  Establish a connection to the session object for a specific
+     *  VPN session, enable session logging and sets the log level.
+     *
+     *  The input path is used to ensure a SESS_CREATED SessionManager::Event
+     *  is tied to the VPN session we expect.
+     *
+     * @param path  std::string of the VPN session to attach to.
+     */
+    void create_session_proxy(std::string path)
+    {
+        if (path.empty()
+            || (0 != session_path.compare(path)))
+        {
+            return;
+        }
+
+        if (wait_notification)
+        {
+            std::cout << " Done" << std::endl;
+        }
+
+        // Check if the session manager will forward log events for this
+        // session.  If not, we must enable it - and if we do this, we
+        // track that we modified this setting.
+        session_proxy.reset(new OpenVPN3SessionProxy(G_BUS_TYPE_SYSTEM, path));
+        if (!session_proxy->CheckObjectExists())
+        {
+            throw CommandException("log",
+                                   "Session not found");
+        }
+
+        std::cout << "Attaching to session " << path << std::endl;
+
+        if (!session_proxy->GetReceiveLogEvents())
+        {
+            session_proxy->SetReceiveLogEvents(true);
+            log_flag_reset = true;
+        }
+
+        if (log_level > 0)
+        {
+            try
+            {
+                session_proxy->SetLogVerbosity(log_level);
+            }
+            catch (std::exception& e)
+            {
+                // Log level might be incorrect, but we don't exit because
+                // of that - as it might be we've been waiting for a session
+                // to start.
+                std::cerr << "** WARNING ** Log level not set: "
+                          << e.what() << std::endl;
+            }
+        }
+        // Setup a Logger object for the provided session path
+        session_log.reset(new SessionLogger(GetConnection(),
+                                            OpenVPN3DBus_interf_sessions,
+                                            session_path, mainloop));
+    }
+
+
+    void cleanup()
+    {
+        // If we enabled the receive_log_events property in a session object,
+        // disable it again when we exit.
+        if (session_proxy && log_flag_reset)
+        {
+            if (session_proxy->CheckObjectExists())
+            {
+                session_proxy->SetReceiveLogEvents(false);
+            }
+        }
+    }
+};
+
+
 /**
  *  openvpn3 log
  *
@@ -188,10 +449,8 @@ static int cmd_log(ParsedArgs args)
     g_unix_signal_add(SIGINT, stop_handler, main_loop);
     g_unix_signal_add(SIGTERM, stop_handler, main_loop);
 
-    SessionLogger::Ptr session_log;
     Logger::Ptr config_log;
-    bool log_flag_reset = false;
-    std::string session_path = "";
+    std::unique_ptr<LogAttach> logattach;
     DBus dbuscon(G_BUS_TYPE_SYSTEM);
     dbuscon.Connect();
 
@@ -199,76 +458,24 @@ static int cmd_log(ParsedArgs args)
         || args.Present("interface"))
     {
         std::string session_path = "";
-        std::unique_ptr<OpenVPN3SessionProxy> sessmgr;
+        logattach.reset(new LogAttach (main_loop, dbuscon.GetConnection()));
 
-        if (args.Present("config") || args.Present("interface"))
+        if (args.Present("log-level"))
         {
-            sessmgr.reset(new OpenVPN3SessionProxy(G_BUS_TYPE_SYSTEM,
-                                                   OpenVPN3DBus_rootp_sessions));
+            logattach->SetLogLevel(std::stoi(args.GetValue("log-level", 0)));
         }
 
         if (args.Present("config"))
         {
-            std::vector<std::string> paths = sessmgr->LookupConfigName(args.GetValue("config", 0));
-            if (0 == paths.size())
-            {
-                throw CommandException("log",
-                                       "No sessions started with this "
-                                       "configuration profile name was found");
-            }
-            else if (1 < paths.size())
-            {
-                throw CommandException("log",
-                                       "More than one session with the given "
-                                       "configuration profile name was found.");
-            }
-            session_path = paths.at(0);
+            logattach->AttchByConfig(args.GetValue("config", 0));
         }
         else if (args.Present("interface"))
         {
-            session_path = sessmgr->LookupInterface(args.GetValue("interface", 0));
+            logattach->AttachByInterface(args.GetValue("interface", 0));
         }
         else
         {
-            session_path = args.GetValue("session-path", 0);
-        }
-
-        std::cout << "Attaching to session " << session_path << std::endl;
-
-        // Check if the session manager will forward log events for this
-        // session.  If not, we must enable it - and if we do this, we
-        // track that we modified this setting.
-        OpenVPN3SessionProxy sesprx(G_BUS_TYPE_SYSTEM, session_path);
-        if (!sesprx.CheckObjectExists())
-        {
-            throw CommandException("log",
-                                   "Session not found");
-        }
-
-        if (!sesprx.GetReceiveLogEvents())
-        {
-            sesprx.SetReceiveLogEvents(true);
-            log_flag_reset = true;
-        }
-        // Setup a Logger object for the provided session path
-        session_log.reset(new SessionLogger(dbuscon.GetConnection(),
-                                            OpenVPN3DBus_interf_sessions,
-                                            session_path, main_loop));
-
-        if (args.Present("log-level"))
-        {
-            try
-            {
-                int loglev = std::stoi(args.GetValue("log-level", 0));
-                session_log->SetLogLevel(loglev);
-                sesprx.SetLogVerbosity(loglev);
-            }
-            catch (std::exception& e)
-            {
-                throw CommandException("log",
-                                       "Invalid --log-level argument: "
-                                       + std::string(e.what()));
-            }
+            logattach->AttachByPath(args.GetValue("session-path", 0));
         }
     }
 
@@ -285,16 +492,6 @@ static int cmd_log(ParsedArgs args)
     // Start the main loop.  This will exit on SIGINT or SIGTERM signals only
     g_main_loop_run(main_loop);
 
-    // If we enabled the receive_log_events property in a session object,
-    // disable it again when we exit.
-    if (!session_path.empty() && log_flag_reset)
-    {
-        OpenVPN3SessionProxy sesprx(G_BUS_TYPE_SYSTEM, session_path);
-        if (sesprx.CheckObjectExists())
-        {
-            sesprx.SetReceiveLogEvents(false);
-        }
-    }
 
     // Clean-up and shut down.
     g_main_loop_unref(main_loop);
