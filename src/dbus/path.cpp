da4f7dd6e97693f3331691d9978cf95fc3756f39
David Sommerseth <davids@openvpn.net>
2022-02-17 13:19:32 +0100

    dbus: Make path.hpp a separate compilation unit
    This is normally a too small file to convert to an independent
    compilation unit. But due to some on-going refactoring, this little step
    simplifies the on-going work.  And it doesn't add any harm anywhere.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/dbus/path.cpp
+++ src/dbus/path.cpp
@@ -17,8 +17,18 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-#pragma once
-
+#include <algorithm>
 #include <string>
+#include <uuid/uuid.h>
 
-std::string generate_path_uuid(std::string prefix, char delim);
+std::string generate_path_uuid(std::string prefix, char delim)
+{
+    uuid_t uuid;
+    char uuid_str[38];
+    uuid_generate_random(uuid);
+    uuid_unparse_lower(uuid, uuid_str);
+    std::string ret(uuid_str);
+    std::replace(ret.begin(), ret.end(), '-', delim);
+
+    return (prefix == "" ? ret : prefix + "/" + ret);
+}
