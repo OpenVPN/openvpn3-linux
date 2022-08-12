708406d5cf1dd8c50e918748f43da11e61565443
David Sommerseth <davids@openvpn.net>
2022-08-12 22:43:56 +0200

    log: Extend state/config file option coverage
    This extends possible settings via the --state-dir configuration file.
    New supported options are:
    
      --journald          (journald)
      --syslog            (syslog)
      --syslog-facility   (syslog_facility)
      --log-file          (log_file)
      --colour            (log_file_colour)
      --idle-exit         (idle_exit)
    
    Identifiers in the brackets are the field names used in the JSON file.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/log/service-configfile.hpp
+++ src/log/service-configfile.hpp
@@ -53,18 +53,39 @@ protected:
     Configuration::OptionMap ConfigureMapping()
     {
         return {
+            OptionMapEntry{"journald", "journald",
+                           "log_method_group",
+                           "Use systemd-journald",
+                           OptionValueType::Present},
+            OptionMapEntry{"syslog", "syslog",
+                           "log_method_group",
+                           "Use syslog",
+                           OptionValueType::Present},
+            OptionMapEntry{"syslog-facility", "syslog_facility",
+                           "Syslog facility",
+                           OptionValueType::String},
+            OptionMapEntry{"log-file", "log_file",
+                           "log_method_group",
+                           "Log file",
+                           OptionValueType::String},
+            OptionMapEntry{"colour", "log_file_colour",
+                           "Colour log lines in log file",
+                           OptionValueType::Present},
             OptionMapEntry{"log-level", "log_level",
                            "Log level",
                            OptionValueType::Int},
             OptionMapEntry{"service-log-dbus-details", "log_dbus_details",
-                           "Add D-Bus debug details related to Log event senders",
+                           "Add D-Bus details in log file/syslog",
                            OptionValueType::Present},
             OptionMapEntry{"timestamp", "timestamp",
-                           "Add timestamp to each Log event lines",
+                           "Add timestamp log file",
                            OptionValueType::Present},
             OptionMapEntry{"no-logtag-prefix", "no_logtag_prefix",
-                           "Disable LogTag prefixes in journald log events",
+                           "Disable LogTag prefixes (systemd-journald)",
                            OptionValueType::Present},
+            OptionMapEntry{"idle-exit", "idle_exit",
+                           "Idle exit timer (minutes)", OptionValueType::Int},
+
             };
     }
 };
