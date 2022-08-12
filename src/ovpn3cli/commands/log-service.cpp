7e2d51322be351f769b111826ae4140d63fdeb6d
David Sommerseth <davids@openvpn.net>
2022-08-13 01:55:40 +0200

    cli/log: Add new options for logger config file management
    So far openvpn3-service-logger has only had some runtime settings which
    would be preserved if the --state-dir option was present.
    
    With this change, the openvpn3-admin log-service command has been
    extended to also be able to manage other openvpn3-service-logger
    settings which requires the process to restart before becoming active.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/ovpn3cli/commands/log-service.cpp
+++ src/ovpn3cli/commands/log-service.cpp
@@ -27,8 +27,146 @@
 #include "dbus/core.hpp"
 #include "dbus/connection-creds.hpp"
 #include "common/cmdargparser.hpp"
+#include "log/service-configfile.hpp"
 #include "log/proxy-log.hpp"
 #include "../arghelpers.hpp"
+
+
+static int manage_config_file(ParsedArgs::Ptr args,
+                              std::string cfgmode)
+{
+    //
+    // Options for managing the log-service configuration file
+    //
+    try
+    {
+        std::string config_file{};
+        LogServiceConfigFile config;
+
+        try
+        {
+            if (!args->Present("config-file-override"))
+            {
+                DBus dbus(G_BUS_TYPE_SYSTEM);
+                dbus.Connect();
+                LogServiceProxy prx(dbus.GetConnection());
+                config_file = prx.GetConfigFile();
+            }
+            else
+            {
+                config_file = args->GetLastValue("config-file-override");
+            }
+            std::cout << "Loading configuration file: " << config_file
+                      << std::endl;
+            config.Load(config_file);
+
+            if ((config.IsPresent("syslog") || config.IsPresent("journald"))
+                && (config.IsPresent("log-file") || config.IsPresent("colour")))
+            {
+                throw ExclusiveOptionError({"syslog", "journald",
+                                            "log-file", "colour"});
+            }
+            config.CheckExclusiveOptions();
+
+        }
+        catch (const ConfigFileException& excp)
+        {
+            if ("config-show" == cfgmode)
+            {
+                std::cout << excp.what()
+                          << std::endl;
+                return 2;
+            }
+        }
+        catch (const ExclusiveOptionError& err)
+        {
+            std::cerr << std::endl
+                      << "==================================="
+                      << "==================================="
+                      << std::endl;
+            std::cerr << "An error was found in the configuration file:"
+                      << std::endl;
+            std::cerr << " *** " << err.what() << std::endl;
+            std::cerr << "==================================="
+                      << "==================================="
+                      << std::endl << std::endl;
+        }
+
+        if ("config-show" == cfgmode)
+        {
+            if (!config.empty())
+            {
+                std::cout << config;
+            }
+            else
+            {
+                std::cout << "Configuration file is empty" << std::endl;
+            }
+        }
+        else if ("config-set" == cfgmode
+                 || "config-unset" == cfgmode)
+        {
+            std::string optname = args->GetLastValue(cfgmode);
+            std::vector<std::string> values = args->GetAllExtraArgs();
+            std::string new_value{""};
+
+            // Simple sanity to retrieve the new config value
+            if ("config-set" == cfgmode && values.size() == 0)
+            {
+                throw CommandException("log-service",
+                                       "A value must be given to --"
+                                       + cfgmode + " " + optname);
+            }
+            else if ("config-unset" == cfgmode && values.size() > 0)
+            {
+                throw CommandException("log-service",
+                                       "No value can be given to --"
+                                       + cfgmode + " " + optname);
+            }
+            else if (values.size() > 1)
+            {
+                throw CommandException("log-service",
+                                       "Only a single value can be given to --"
+                                       + cfgmode + " " + optname);
+            }
+            else if (values.size() == 1)
+            {
+                new_value = values.at(0);
+            }
+
+            // Update the configuration and save the file.
+            if ("config-unset" == cfgmode)
+            {
+                config.UnsetOption(optname);
+            }
+            else
+            {
+                config.SetValue(optname, new_value);
+            }
+            try
+            {
+                config.CheckExclusiveOptions();
+                config.Save(config_file);
+                std::cout << "Configuration file updated.  "
+                          << "Changes will be activated next time "
+                          << "openvpn3-service-logger restarts"
+                          << std::endl;
+            }
+            catch (const ExclusiveOptionError& err)
+            {
+                std::cerr << "Configuration NOT changed due to the "
+                          << "following error:" << std::endl << std::endl;
+                std::cerr << " *** " << err.what() << std::endl
+                          << std::endl;
+            }
+        }
+    }
+    catch (const OptionNotFound&)
+    {
+        // Nothing to do; options not present
+    }
+    return 0;
+}
 
 /**
  *  openvpn3 log-service
@@ -46,6 +184,19 @@ static int cmd_log_service(ParsedArgs::Ptr args)
 {
     try
     {
+        try
+        {
+            std::vector<std::string> cfgopts = {"config-show",
+                                                "config-set", "config-unset"};
+            std::string cfgmode = args->Present(cfgopts);
+            return manage_config_file(args, cfgmode);
+        }
+        catch (const OptionNotFound&)
+        {
+            // If no config file related options were found,
+            // continue with the runtime configurations
+        }
+
         DBus dbus(G_BUS_TYPE_SYSTEM);
         dbus.Connect();
         LogServiceProxy logsrvprx(dbus.GetConnection());
@@ -208,6 +359,29 @@ static int cmd_log_service(ParsedArgs::Ptr args)
 }
 
 
+static std::string arghelper_logger_config_keys()
+{
+    LogServiceConfigFile cfg("");
+    std::stringstream r;
+
+    // These options are already handled differently, and does not require
+    // a restart of the service
+    std::vector<std::string> ignore = {
+            "log-level", "timestamp", "service-log-dbus-details",
+            "no-logtag-prefix"
+    };
+
+    for (const auto& o : cfg.GetOptions(true))
+    {
+        if (std::find(ignore.begin(), ignore.end(), o) == ignore.end())
+        {
+            r << o << " ";
+        }
+    }
+    return r.str();
+}
+
+
 SingleCommand::Ptr prepare_command_log_service()
 {
     SingleCommand::Ptr cmd;
@@ -228,6 +402,18 @@ SingleCommand::Ptr prepare_command_log_service()
                    arghelper_boolean);
     cmd->AddOption("list-subscriptions",
                    "List all subscriptions which has attached to the log service");
+    cmd->AddOption("config-show",
+                   "Show the current configuration file used by log-service");
+    cmd->AddOption("config-set", 0, "CONFIG-KEY", true,
+                   "Sets a configuration option and saves it in the config file",
+                   arghelper_logger_config_keys);
+    cmd->AddOption("config-unset", 0, "CONFIG-KEY", true,
+                   "Removes a configuration option and updates the config file",
+                   arghelper_logger_config_keys);
+    cmd->AddOption("config-file-override", 0, "CONFIG-FILE", true,
+                   "Overrides the default configuration file (default file "
+                   "provivded by openvpn3-service-logger)");
+
 
     return cmd;
 }
