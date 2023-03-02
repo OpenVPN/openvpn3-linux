a133a495f886045692207341c2a1e46fbab3dab5
David Sommerseth <davids@openvpn.net>
2023-03-02 22:05:10 +0100

    ovpn3cli/admin: Make init-config also check for configs subdir
    When the openvpn3-admin init-config checked for the state directory, it
    didn't check the 'configs' subdirectory.  Include a check for this as
    well as creating it if missing.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/ovpn3cli/commands/init-config.cpp
+++ src/ovpn3cli/commands/init-config.cpp
@@ -398,10 +398,10 @@ void setup_state_dir(const setup_config &setupcfg)
 
     if (!is_directory(setupcfg.statedir, true))
     {
-        std::cout << "    -- Configuration is missing - creating it" << std::endl;
+        std::cout << "    -- State directory is missing - creating it" << std::endl;
         if (0 != ::mkdir(setupcfg.statedir.c_str(), S_IRWXU | S_IRGRP | S_IXGRP))
         {
-            throw CommandException("init-setup", "Failed to create a new state/configuration directory");
+            throw CommandException("init-setup", "Failed to create a new state directory");
         }
     }
     else
@@ -409,13 +409,28 @@ void setup_state_dir(const setup_config &setupcfg)
         std::cout << "    Directory found" << std::endl;
         if (0 != ::chmod(setupcfg.statedir.c_str(), S_IRWXU | S_IRGRP | S_IXGRP))
         {
-            throw CommandException("init-setup", "Failed fixing access mode to state/configuration directory (chmod)");
+            throw CommandException("init-setup", "Failed fixing access mode to state directory (chmod)");
         }
     }
 
     if (0 != ::chown(setupcfg.statedir.c_str(), setupcfg.openvpn_uid, setupcfg.openvpn_gid))
     {
-        throw CommandException("init-setup", "Failed to change ownership of state/configuration directory");
+        throw CommandException("init-setup", "Failed to change ownership of state directory");
+    }
+
+    std::string cfgdir = setupcfg.statedir + "/configs";
+    if (!is_directory(cfgdir, true))
+    {
+        std::cout << "    -- Configurations sub-directory is missing - creating it" << std::endl;
+        if (0 != ::mkdir(cfgdir.c_str(), S_IRWXU | S_IRGRP | S_IXGRP))
+        {
+            throw CommandException("init-setup", "Failed to create a new configurations sub-directory");
+        }
+    }
+
+    if (0 != ::chown(cfgdir.c_str(), setupcfg.openvpn_uid, setupcfg.openvpn_gid))
+    {
+        throw CommandException("init-setup", "Failed to change ownership of configurations sub-directory");
     }
 }
 
