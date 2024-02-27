755088726654694c39adfe622d55b6947afac59e
David Sommerseth <davids@openvpn.net>
2024-02-27 20:50:56 +0100

    netcfg/build: Include NetCfgChangeType into the static netcfg library
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/netcfg/netcfg-changetype.cpp
+++ src/netcfg/netcfg-changetype.cpp
@@ -2,9 +2,9 @@
 //
 //  SPDX-License-Identifier: AGPL-3.0-only
 //
-//  Copyright (C) 2019 - 2023  OpenVPN Inc <sales@openvpn.net>
-//  Copyright (C) 2019 - 2023  Lev Stipakov <lev@openvpn.net>
-//  Copyright (C) 2019 - 2023  David Sommerseth <davids@openvpn.net>
+//  Copyright (C) 2019-  OpenVPN Inc <sales@openvpn.net>
+//  Copyright (C) 2019-  Lev Stipakov <lev@openvpn.net>
+//  Copyright (C) 2019-  David Sommerseth <davids@openvpn.net>
 //
 
 /**
