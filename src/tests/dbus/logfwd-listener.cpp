dda1e922e1b4d923d7ae626c1474ab7b50f329fe
David Sommerseth <davids@openvpn.net>
2024-05-23 12:25:35 +0200

    tests/GDBus++: Migrate the logfwd-listener test program
    This migrates the logfwd-listener to use the GDBus++ library.
    
    The logging functionality has been re-implemented to build on the
    LogWriter API which gives simpler approaches for the log output
    and event parsing.
    
    In addition, it enables using the ColourStreamWriter for more
    colourful log output.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- src/tests/dbus/logfwd-listener.cpp
+++ src/tests/dbus/logfwd-listener.cpp
@@ -2,8 +2,8 @@
 //
 //  SPDX-License-Identifier: AGPL-3.0-only
 //
-//  Copyright (C) 2022 - 2023  OpenVPN Inc <sales@openvpn.net>
-//  Copyright (C) 2022 - 2023  David Sommerseth <davids@openvpn.net>
+//  Copyright (C) 2022  OpenVPN Inc <sales@openvpn.net>
+//  Copyright (C) 2022  David Sommerseth <davids@openvpn.net>
 //
 
 /**
@@ -16,82 +16,94 @@
 
 #include <iostream>
 #include <string.h>
+#include <gdbuspp/connection.hpp>
+#include <gdbuspp/mainloop.hpp>
+#include <gdbuspp/proxy/utils.hpp>
+#include <gdbuspp/signals/subscriptionmgr.hpp>
+#include <gdbuspp/signals/target.hpp>
 
-#include <glib-unix.h>
-
-#include "dbus/core.hpp"
-#include "client/statusevent.hpp"
 #include "common/utils.hpp"
-#include "log/dbus-logfwd.hpp"
+#include "dbus/constants.hpp"
+#include "dbus/signals/log.hpp"
+#include "dbus/signals/statuschange.hpp"
+#include "log/ansicolours.hpp"
+#include "log/logwriter.hpp"
+#include "log/logwriters/implementations.hpp"
 
-using namespace openvpn;
+#include "sessionmgr/proxy-sessionmgr.hpp"
 
-
-class LogFwdSubscription : public LogForwardBase<LogFwdSubscription>
+class EventLogger
 {
   public:
-    LogFwdSubscription(GMainLoop *mainloop,
-                       GDBusConnection *dbc,
-                       const std::string &path,
-                       const std::string &interf = "")
-        : LogForwardBase(dbc, interf, path),
-          main_loop(mainloop)
-    {
-    }
-
-  protected:
-    void ConsumeLogEvent(const std::string sender_name,
-                         const std::string interface_name,
-                         const std::string obj_path,
-                         const Log &logev) final
-    {
-        std::cout << " LogEvent{sender=" << sender_name
-                  << ", interface=" << interface_name
-                  << ", path=" << obj_path << "} : " << logev << std::endl;
-    }
-
-    void StatusChangeEvent(const std::string sender_name,
-                           const std::string interface_name,
-                           const std::string obj_path,
-                           const StatusEvent &status) final
-    {
-        std::cout << " StatusChange{sender=" << sender_name
-                  << ", interface=" << interface_name
-                  << ", path=" << obj_path << "} : " << status << std::endl;
-
-        if (status.Check(StatusMajor::CONNECTION, StatusMinor::CONN_DISCONNECTED))
-        {
-            if (main_loop)
-            {
-                g_main_loop_quit(main_loop);
-            }
-        }
-    }
+    EventLogger(DBus::MainLoop::Ptr mainl,
+                LogWriter::Ptr logdst,
+                DBus::Connection::Ptr conn,
+                const DBus::Object::Path &session_path);
 
   private:
-    GMainLoop *main_loop = nullptr;
+    DBus::MainLoop::Ptr mainloop = nullptr;
+    LogWriter::Ptr logdest = nullptr;
+    DBus::Signals::SubscriptionManager::Ptr submgr = nullptr;
+    DBus::Signals::Target::Ptr signal_sender = nullptr;
+    Signals::ReceiveLog::Ptr sighandler_log = nullptr;
+    Signals::ReceiveStatusChange::Ptr sighandler_statuschg = nullptr;
 };
 
+
+EventLogger::EventLogger(DBus::MainLoop::Ptr mainl,
+                         LogWriter::Ptr logdst,
+                         DBus::Connection::Ptr conn,
+                         const DBus::Object::Path &session_path)
+    : mainloop(mainl), logdest(logdst)
+{
+    auto prxqry = DBus::Proxy::Utils::DBusServiceQuery::Create(conn);
+    auto log_busname = prxqry->GetNameOwner(Constants::GenServiceName("log"));
+
+    submgr = DBus::Signals::SubscriptionManager::Create(conn);
+    signal_sender = DBus::Signals::Target::Create(log_busname, session_path, "");
+
+    sighandler_log = Signals::ReceiveLog::Create(
+        submgr,
+        signal_sender,
+        [&](const Events::Log logev)
+        {
+            Events::Log event(logev);
+            event.message = logev.str(26);
+            logdest->Write(event);
+        });
+
+    sighandler_statuschg = Signals::ReceiveStatusChange::Create(
+        submgr,
+        signal_sender,
+        [&](const std::string &sender, const DBus::Object::Path &path, const std::string &interface, Events::Status stchgev)
+        {
+            logdest->WriteStatus(stchgev);
+            if (StatusMajor::CONNECTION == stchgev.major
+                && (StatusMinor::CONN_DONE == stchgev.minor))
+            {
+                logdest->Write("*** Stopping mainloop");
+                mainloop->Stop();
+            }
+        });
+}
 
 
 int main(int argc, char **argv)
 {
     if (argc != 3)
     {
-        std::cerr << "Usage: " << argv[0] << " <--config|--session-path> <config name>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " <--config|--session-path> <config name|session path>" << std::endl;
         return 1;
     }
-
-    DBus dbus(G_BUS_TYPE_SYSTEM);
-    dbus.Connect();
 
     std::string mode{argv[1]};
     std::string session_path{};
 
-    OpenVPN3SessionMgrProxy mgr(dbus.GetConnection());
+    auto dbuscon = DBus::Connection::Create(DBus::BusType::SYSTEM);
+    auto mgr = SessionManager::Proxy::Manager::Create(dbuscon);
     if ("--config" == mode)
     {
-        std::vector<std::string> sp = mgr.LookupConfigName(argv[2]);
+        auto sp = mgr->LookupConfigName(argv[2]);
         if (sp.size() > 1)
         {
             std::cerr << "More than one session was found with '" << argv[2]
@@ -108,7 +120,7 @@ int main(int argc, char **argv)
     }
     else if ("--session-path" == mode)
     {
-        session_path = std::string(argv[2]);
+        session_path = DBus::Object::Path(argv[2]);
     }
     else
     {
@@ -116,22 +128,39 @@ int main(int argc, char **argv)
         return 2;
     }
 
-    GMainLoop *main_loop = g_main_loop_new(NULL, FALSE);
-    g_unix_signal_add(SIGINT, stop_handler, main_loop);
-    g_unix_signal_add(SIGTERM, stop_handler, main_loop);
-
     std::cout << "Attaching to session path: " << session_path << std::endl;
+    auto session = mgr->Retrieve(session_path);
+
+    std::ostream logdest(std::cout.rdbuf());
+    LogWriter::Ptr logwr = nullptr;
+    ColourEngine::Ptr colourengine = nullptr;
+    if (is_colour_terminal())
+    {
+        colourengine.reset(new ANSIColours());
+        logwr.reset(new ColourStreamWriter(logdest, colourengine.get()));
+    }
+    else
+    {
+        logwr.reset(new StreamLogWriter(logdest));
+    }
+
     try
     {
-        auto logsub = LogFwdSubscription::create(main_loop, dbus.GetConnection(), session_path);
-        std::cout << "Log level: " << std::to_string(logsub->GetLogLevel()) << std::endl;
-        std::cout << "D-Bus bus name: " << dbus.GetUniqueBusName() << std::endl;
-        g_main_loop_run(main_loop);
+        std::cout << "D-Bus bus name: " << dbuscon->GetUniqueBusName() << std::endl;
+        std::cout << "Log level: " << std::to_string(session->GetLogVerbosity()) << std::endl;
+
+
+        auto mainloop = DBus::MainLoop::Create();
+        EventLogger logger(mainloop, logwr, dbuscon, session_path);
+
+        session->LogForward(true);
+        mainloop->Run();
+        session->LogForward(false);
     }
-    catch (const DBusProxyAccessDeniedException &e)
+    catch (const DBus::Exception &e)
     {
+        session->LogForward(false);
         std::cerr << "** ERROR ** " << e.what() << std::endl;
-        std::cerr << "            " << e.getDebug() << std::endl;
         return 1;
     }
 
